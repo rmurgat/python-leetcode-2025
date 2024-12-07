124667953deb54f038a19466864138e537af0dc6
Ruben Murga <ruben.murgat@gmail.com>
2024-12-06 21:07:17 -0600

    updating code

--- menuPractice1.py
+++ menuPractice1.py
@@ -4,12 +4,29 @@ def printingSumTwo():
     lib = ListBundle01()
     print ("Sum Two: ", lib.twoSum([2,7,11,15],9))
 
+def printingContainsDuplicate():
+    lib = ListBundle01()
+    print ("[Contain Duplicates] ")
+    print ("Answer #1: ", lib.hasDuplicate_1([1,2,3,1]))
+    print ("Answer #2: ", lib.hasDuplicate_1([1,2,3,4]))
+
+def printingValidAnagram():
+    lib = ListBundle01()
+    print ("[Valid Anagram] ")
+    print ("Answer #1: ", lib.isAnagram("anagram", "nagaram"))
+
+
+
 def menu():
     print("Menu Practice Python No. 1")
     print("1. Two Sum (LC#1)")
+    print("2. Contains Duplicate (LC#217)")
+    print("3. Valid Anagram (LC#242)")
     opc = int(input("Enter Option?"))
     match opc:
         case 1: printingSumTwo()
+        case 2: printingContainsDuplicate()
+        case 3: printingValidAnagram()
         case _: print("Error, option not recognized")
 
 menu()
--- solutions/ListBunble01.py
+++ solutions/ListBunble01.py
@@ -15,5 +15,27 @@ class ListBundle01:
             memo[nums[i]] = i
 
         return [] 
+    
+    def hasDuplicate_1(self, nums: List[int]) -> bool:
+        s = set(nums)
+        return True if len(s)!=len(nums) else False
+    
+    def isAnagram(self, s: str, t: str) -> bool:
+        d = {}
+        for c in s:
+            located = d.get(c,None)
+            if not located is None:
+                d[c] = located+1
+            else:
+                d[c] = 1
+        for c in t:
+            located = d.get(c,None)
+            if not located is None:
+                d[c] = d[c]-1
+            else:
+                return False
+        dif = [x for x,y in d.items() if y!=0]
+        return True if len(dif)==0 else False
+                
 
         
