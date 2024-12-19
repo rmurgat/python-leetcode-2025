532e77eb2ceed466b7a18f8c96a98c9e52ed9321
Ruben Murga <ruben.murgat@gmail.com>
2024-12-19 17:22:40 -0600

    updating code

--- menuPractice1.py
+++ menuPractice1.py
@@ -15,6 +15,62 @@ def printingValidAnagram():
     print ("[Valid Anagram] ")
     print ("Answer #1: ", lib.isAnagram("anagram", "nagaram"))
 
+def printingGroupAnagram():
+    lib = ListBundle01()
+    print ("[Group Anagram]")
+    print ("Answer No. 1", lib.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
+def printingReplaceElements():
+    lib = ListBundle01()
+    print ("[Replace Element]")
+    print ("Answer No. 1", lib.replaceElements([17,18,5,4,6,1]))
+
+def printingIsSequence():
+    lib = ListBundle01()
+    print ("[Is Sequence]")
+    print ("Answer No. 1: ", lib.isSubsequence("abc","ahbgdc"))
+
+def printingLengthLastWord():
+    lib = ListBundle01()
+    print ("[Is Sequence]")
+    print ("Answer No. 1: ", lib.lengthOfLastWord("   fly me   to   the moon  "))
+    print ("Answer No. 2: ", lib.lengthOfLastWord("Hello World"))
+    print ("Answer No. 3: ", lib.lengthOfLastWord("luffy is still joyboy"))
+    print ("Answer No. 4: ", lib.lengthOfLastWord("luffy"))
+    print ("Answer No. 5: ", lib.lengthOfLastWord(""))
+
+def printingLongestCommonPrefix():
+    lib = ListBundle01()
+    print ("[ Longest Common Prefix ]")
+    print ("Answer No. 1, type 1: ", lib.longestCommonPrefix_1(["flower","flow","flight"]))
+    print ("Answer No. 2, type 1: ", lib.longestCommonPrefix_1(["dog","racecar","car"]))
+    print ("Answer No. 3, type 1: ", lib.longestCommonPrefix_1(["car"]))
+    print ("Answer No. 4, type 1: ", lib.longestCommonPrefix_1([""]))
+    print ("Answer No. 1, type 2: ", lib.longestCommonPrefix_2(["flower","flow","flight"]))
+    print ("Answer No. 2, type 2: ", lib.longestCommonPrefix_2(["dog","racecar","car"]))
+    print ("Answer No. 3, type 2: ", lib.longestCommonPrefix_2(["car"]))
+    print ("Answer No. 4, type 2: ", lib.longestCommonPrefix_2([""]))
+
+def printingTopKFreqElement():
+    lib = ListBundle01()
+    print ("[ Top K Frequenct Element ]")
+    print (" Answer 1, type 1 ", lib.topKFrequent_1([1,1,1,2,2,3],2))
+    print (" Answer 2, type 1 ", lib.topKFrequent_1([1],1))
+
+def printingEncodeDecode():
+    lib = ListBundle01()
+    print ("[ Encode / Decode Strings ]")
+    a = ["neet","code","love","you"]
+    s = lib.encode(a)
+    print ("Answer 1 (encode): ", s)
+    b = lib.decode(s)
+    print ("Answer 1 (decode): ", b)
+
+def printingArrayExceptSelf():
+    lib = ListBundle01()
+    print ("[ Array Except Self ]")
+    nums = [1,2,3,4]
+    print ("Answer #1: ", lib.productExceptSelf(nums))
 
 
 def menu():
@@ -22,11 +78,27 @@ def menu():
     print("1. Two Sum (LC#1)")
     print("2. Contains Duplicate (LC#217)")
     print("3. Valid Anagram (LC#242)")
-    opc = int(input("Enter Option?"))
+    print("4. Group Anagram (LC#49)")
+    print("5. Replace Elements (LC#1299)")
+    print("6. Is Sequence (LC#392)")
+    print("7. Length of Last Word (LC#58)")
+    print("8. Longest Common prefix (lc#14)")
+    print("9. Top K Frequent Element (LC#347)")
+    print("10. Encode and Decode String (neetcode)")
+    print("11. Product of Array Except Self (LC#238)")
+    opc = int(input("Enter Option? "))
     match opc:
         case 1: printingSumTwo()
         case 2: printingContainsDuplicate()
         case 3: printingValidAnagram()
+        case 4: printingGroupAnagram()
+        case 5: printingReplaceElements()
+        case 6: printingIsSequence()
+        case 7: printingLengthLastWord()
+        case 8: printingLongestCommonPrefix()
+        case 9: printingTopKFreqElement()
+        case 10: printingEncodeDecode()
+        case 11: printingArrayExceptSelf()
         case _: print("Error, option not recognized")
 
 menu()
--- solutions/ListBunble01.py
+++ solutions/ListBunble01.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 
@@ -36,6 +37,105 @@ class ListBundle01:
                 return False
         dif = [x for x,y in d.items() if y!=0]
         return True if len(dif)==0 else False
-                
-
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for s in strs:
+            ss = "".join(sorted(s))
+            if ss in d:
+                d[ss].append(s)
+            else:
+                d[ss] = [s]
+        return [x for x in d.values()]
         
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        ans = []
+        L = len(arr)
+        max1 = -1
+        for i in range(L-1,-1,-1):
+            ans.append(max1)
+            max1 = max(max1, arr[i])
+        return ans[::-1]
+                
+    def isSubsequence(self, s: str, t: str) -> bool:
+        L = len(s)
+        i = 0
+        j = 0
+        located = 0
+        while j < len(t):
+            if located == L: return True
+            if s[i]==t[j]:
+                i+=1;
+                located+=1
+            j+=1
+        return True if located==L else False  
+        
+    def lengthOfLastWord(self, s: str) -> int:
+        snew = s.rstrip();
+        j = len(snew)-1
+        while j>-1:
+            if snew[j]==' ': break;
+            j-=1
+        return (len(snew)-1)-j
+    
+    def longestCommonPrefix_1(self, strs: List[str]) -> str:
+        ans = "";
+        sstrs = sorted(strs)
+        left = sstrs[0]
+        right = sstrs[-1]
+        for i in range(min(len(left), len(right))):
+            if left[i]!=right[i]:
+                break
+            ans+=left[i]
+        return ans
+
+    def longestCommonPrefix_2(self, strs: List[str]) -> str:
+        if strs is None or len(strs)==0: return ""
+        prefix = strs[0]
+        for s in strs:
+            while not s.startswith(prefix):
+                prefix = prefix[0:len(prefix)-1]
+        return prefix
+    
+    def topKFrequent_1(self, nums: List[int], k: int) -> List[int]:
+        d = {}
+        for n in nums:
+            d[n]=d.get(n,0)+1
+        sd = dict(reversed(sorted(d.items(), key=lambda item: item[1])))
+        return list(sd)[:k]
+    
+    def encode(self, strs: List[str]) -> str:
+        ans = ""
+        for s in strs:
+            for c in s:
+                i = ord(c) << 2
+                ans += chr(i) 
+            ans += chr(10) 
+        return ans 
+
+    def decode(self, s: str) -> List[str]:
+        ans = []
+        element = ""
+        for c in s:
+            if c == chr(10):
+                ans.append(element)
+                element = ""
+            else:
+                i = ord(c) >> 2
+                element += chr(i)
+        return ans
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [0] * len(nums)
+
+        left = 1
+        for i in range(len(nums)):
+            ans[i] = left
+            left = left * nums[i]
+
+        right = 1
+        for i in reversed(range(len(nums))):
+            ans[i] = right * ans[i]
+            right = right * nums[i]
+
+        return ans
